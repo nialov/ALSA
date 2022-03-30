a0e4d3b0c55ca157a55b84826c7341137f747f55
nialov <nikolasovaskainen@gmail.com>
2022-03-30 16:55:10 +0300

    test: improve and fix tests

--- tests/test_crack_main.py
+++ tests/test_crack_main.py
@@ -9,7 +9,7 @@ import pytest
 from shapely.geometry import LineString, MultiLineString
 
 import tests
-from alsa import crack_main
+from alsa import crack_cls, crack_main
 
 
 @pytest.mark.skipif(
@@ -22,13 +22,15 @@ def test_crack_main(tmp_path):
     traces_path = tmp_path / "test_crack_main_traces.shp"
     if not tests.KL5_TEST_WEIGHTS.exists():
         pytest.xfail("Skipping test_crack_main as weights are missing.")
-    nworks, orig_dims, geo_data, result_gdf = crack_main.crack_main(
+    combined_nwork, orig_dims, geo_data, result_gdf = crack_main.crack_main(
         work_dir=tmp_path,
         img_path=tests.KL5_TEST_IMAGE,
-        area_shp_file_path=list(tests.KL5_TEST_AREA_DIR.glob("*.shp"))[0],
+        area_file_path=list(tests.KL5_TEST_AREA_DIR.glob("*.shp"))[0],
         unet_weights_path=tests.KL5_TEST_WEIGHTS,
         predicted_output_path=traces_path,
     )
+    assert isinstance(combined_nwork, crack_cls.CrackNetWork)
+    assert isinstance(geo_data, gpd.GeoDataFrame)
     assert traces_path.exists()
     assert isinstance(result_gdf, gpd.GeoDataFrame)
     assert len(orig_dims) == 2
