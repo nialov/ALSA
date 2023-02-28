3ecb34a82573c68d43503dbaf3c321eedaa1a39d
nialov <nikolasovaskainen@gmail.com>
2023-02-28 16:22:33 +0200

    feat(scripts): implement all data loading

--- scripts/reproduce.py
+++ scripts/reproduce.py
@@ -15,6 +15,10 @@ TRACES_DIR_NAME = Path(TRACES_ZIP_NAME).stem
 TRACES_DIR_TRACES = "data-exported-ESRI-Shapefile/loviisa/traces/20m/"
 TRACES_DIR_AREAS = "data-exported-ESRI-Shapefile/loviisa/area/20m/"
 
+IMAGES_URL = "https://zenodo.org/record/7077519/files/Loviisa_orthomosaics_for_automation.zip?download=1"
+IMAGES_ZIP_NAME = "Loviisa_orthomosaics_for_automation.zip"
+IMAGES_DIR_NAME = Path(IMAGES_ZIP_NAME).stem
+
 # From alsa/crack_train.py
 TRAINING_IMAGES = Path("Training/Images/Originals")
 TRAINING_AREAS = Path("Training/Shapefiles/Areas")
@@ -101,11 +105,15 @@ TRACE_AREA_IMAGE_DATA_PAIRS = [
 ]
 
 
-def load_traces(reproduction_dir_path: Path):
+def _load_traces(reproduction_dir_path: Path):
     traces_zip_path = reproduction_dir_path / DOWNLOADS_DIR_NAME / TRACES_ZIP_NAME
     traces_zip_path.parent.mkdir(parents=True, exist_ok=True)
     traces_dir_path = reproduction_dir_path / DOWNLOADS_DIR_NAME / TRACES_DIR_NAME
-    subprocess.check_call(["wget", TRACES_URL, f"--output-document={traces_zip_path}"])
+    subprocess.check_call(
+        ["wget", "--continue", TRACES_URL, f"--output-document={traces_zip_path}"]
+    )
+    if traces_dir_path.exists():
+        shutil.rmtree(traces_dir_path)
     traces_dir_path.mkdir()
     shutil.unpack_archive(filename=traces_zip_path, extract_dir=traces_dir_path)
 
@@ -158,13 +166,50 @@ def load_traces(reproduction_dir_path: Path):
             # print(dict(glob_pattern=glob_pattern, src_path=src_path, out_path=out_path))
             # Copy traces
             for shp_file_part in src_path.glob(glob_pattern):
-                out_file_path = out_path / shp_file_part.name
-                # print(dict(shp_file_part=shp_file_part, out_file_path=out_file_path))
+                out_file_path = out_path / shp_file_part.name.lower()
+                out_file_path.unlink(missing_ok=True)
                 shutil.copy(src=shp_file_part, dst=out_file_path)
 
 
+def _load_orthomosaics(reproduction_dir_path: Path):
+    images_zip_path = reproduction_dir_path / DOWNLOADS_DIR_NAME / IMAGES_ZIP_NAME
+    images_zip_path.parent.mkdir(parents=True, exist_ok=True)
+    subprocess.check_call(
+        ["wget", "--continue", IMAGES_URL, f"--output-document={images_zip_path}"]
+    )
+
+    images_dir_path = reproduction_dir_path / DOWNLOADS_DIR_NAME / IMAGES_DIR_NAME
+    images_dir_path.mkdir(exist_ok=True, parents=True)
+    shutil.unpack_archive(filename=images_zip_path, extract_dir=images_dir_path)
+
+    out_training_images_path = reproduction_dir_path / TRAINING_IMAGES
+    out_validation_images_path = reproduction_dir_path / VALIDATION_IMAGES
+    out_prediction_images_path = reproduction_dir_path / PREDICTION_IMAGES
+
+    for data in TRACE_AREA_IMAGE_DATA_PAIRS:
+        if data.data_type is DataType.TRAINING:
+            out_image_path = out_training_images_path
+        elif data.data_type is DataType.VALIDATION:
+            out_image_path = out_validation_images_path
+        elif data.data_type is DataType.PREDICTION:
+            out_image_path = out_prediction_images_path
+        else:
+            raise ValueError(f"Expected DataType enum type.")
+
+        src_image_path = images_dir_path / data.image
+        out_image_path.mkdir(exist_ok=True, parents=True)
+        dst_image_stem = src_image_path.stem[0:3].lower()
+        dst_image_name = f"{dst_image_stem}{src_image_path.suffix}"
+        dst = out_image_path / dst_image_name
+        if not dst.exists():
+            print(f"Moving {src_image_path} to {dst}.")
+            shutil.move(src=src_image_path, dst=dst)
+        src_image_path.unlink(missing_ok=True)
+
+
 def main(reproduction_dir_path: Path):
-    load_traces(reproduction_dir_path=reproduction_dir_path)
+    _load_traces(reproduction_dir_path=reproduction_dir_path)
+    _load_orthomosaics(reproduction_dir_path=reproduction_dir_path)
 
 
 if __name__ == "__main__":
